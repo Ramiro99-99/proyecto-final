a7d55b9fea9e5b89f87ae5739b826213def16031
Ramiro Dominguez <ramiroezequield09@gmail.com>
2025-08-13 19:02:27 -0300

    add:contenido estatico del dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,11 +1,34 @@
 import { Layout } from "../components/Layout"
 
+
+
 const Dashboard = () => {
   return (
     <Layout>
-        <h1>hola desde Dashboard</h1>
-      
+      <h1>Panel de Control</h1>
+      <section>
+        <h2>Agregar nuevo producto</h2>
+        <form>
+          <div>
+            <label>Nombre:</label>
+            <input type="text" placeholder="Ingrese el nombre del producto" />
+          </div>
+
+          <div>
+            <label>Precio:</label>
+            <input type="number" placeholder="Ingrese el precio" />
+          </div>
+
+          <div>
+            <label>Descripción:</label>
+            <textarea placeholder="Ingrese una breve descripción"></textarea>
+          </div>
+
+          <button type="submit">Guardar producto</button>
+        </form>
+      </section>
     </Layout>
   )
 }
-export {Dashboard}
+
+export { Dashboard }
