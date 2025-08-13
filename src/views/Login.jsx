65795025fd79e4bc708f00b51c53b024242253b8
Ramiro Dominguez <ramiroezequield09@gmail.com>
2025-08-13 19:09:20 -0300

    add:Contenido estatico del Login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,8 +3,26 @@ import { Layout } from "../components/Layout"
 const Login = () => {
   return (
     <Layout>
-      <h1>hola desde Login </h1>
+      <h1>Inicia Sesión </h1>
 
+      <section>
+        <h2>Bienvenido al Login </h2>
+        <form>
+          <div>
+            <label>Correo Electronico:</label>
+            <input type="text" placeholder="Ingrese el nombre del producto" />
+          </div>
+
+
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" placeholder="Ingrese el nombre del producto" />
+          </div>
+        </form>
+
+        
+
+      </section>
     </Layout>
   )
 }
