eaa1a1a38bdd990a22b057dbcb45b82a76f55850
Ramiro Dominguez <ramiroezequield09@gmail.com>
2025-08-14 17:05:14 -0300

    add:validaciones del dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,30 +1,87 @@
+import { useState } from "react"
 import { Layout } from "../components/Layout"
 
 
 
 const Dashboard = () => {
+
+  const [nombre, setNombre] = useState("")
+  const[precio,setPrecio]=useState("")
+  const[descripcion,setDescripcion]=useState("")
+  const [error, setError] = useState("")
+  const[succes,setSucces]=useState("")
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+        setError("")
+        setSucces("")
+    
+    
+    
+    
+    if (!nombre || !precio || !descripcion) {
+      setError("Debe colocar obligatoriamente los tres valores")
+      return
+    }
+    
+    
+    
+    setSucces("Su producto fue registrado")
+    
+    setNombre("")
+    setPrecio("")
+    setDescripcion("")
+    
+  }
+
+  {/* el evento es E y setNombre va a capturar el valor del evento E*/}
+  const handleNombre =(e)=>{
+  setNombre(e.target.value)
+}
+
+  const handlePrecio =(e)=>{
+  setPrecio(e.target.value)
+}
+  
+  const handleDescripcion= (e) => {
+  setDescripcion(e.target.value)
+}
+
+
+  
   return (
     <Layout>
       <h1>Panel de Control</h1>
       <section>
         <h2>Agregar nuevo producto</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Nombre:</label>
-            <input type="text" placeholder="Ingrese el nombre del producto" />
+            <input type="text" placeholder="Ingrese el nombre del producto" onChange={handleNombre} value={nombre} />
           </div>
 
           <div>
             <label>Precio:</label>
-            <input type="number" placeholder="Ingrese el precio" />
+            <input type="number" placeholder="Ingrese el precio" onChange={handlePrecio} value={precio} />
           </div>
 
           <div>
             <label>Descripción:</label>
-            <textarea placeholder="Ingrese una breve descripción"></textarea>
+            <textarea placeholder="Ingrese una breve descripción" onChange={handleDescripcion} value={descripcion}></textarea>
           </div>
 
+
           <button type="submit">Guardar producto</button>
+
+          {
+            error && <p style={{color:"red"}}>{error }</p>
+          }
+          {
+            succes && <p style={{color:"slategrey"}}>{ succes}</p>
+          }
+
         </form>
       </section>
     </Layout>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -23,7 +23,7 @@ const handleSubmit = (e) => {
         email,
         password
       }
-      
+      console.log(newUser)
       
       
       
